2bedd590ceb5dcd01ef8cafc83f3063d22e770fd
pekshevds <pekshev.ds@gmail.com>
2022-09-07 14:24:03 +0300

    using enum to optimize selection

--- track.py
+++ track.py
@@ -1,13 +1,19 @@
 import argparse
 import database
 
+from enum import Enum
+
+class Action(Enum):
+    track = 'track'
+    stat = 'stat'
+
 def init_parser():
     """
     parser initialization
     """
     parser = argparse.ArgumentParser(description="Трекинг времени на разные проекты")
-
-    parser.add_argument('action', help='Возможные действия [track - для регистрации времени работы (указывается в параметре --min), stat - для вывода статистики за период (указывается в параметре --days)]')
+    
+    parser.add_argument('action', help='Возможные действия [track - для регистрации времени работы (указывается в параметре --min), stat - для вывода статистики за период (указывается в параметре --days)]', choices=list(Action), type=Action)
     parser.add_argument('--project', help='Имя проекта')
     parser.add_argument('--min', type=int, help='Рабочее время потраченое на проект, в минутах', default=15)
     parser.add_argument('--days', type=int, help='Период вывода статистики, в днях (N-последних дней)', default=5)
@@ -25,15 +31,11 @@ def show_statistic_data(statistic_data = []):
 if __name__ == "__main__":
     
     parser = init_parser()
-
-    args = vars(parser.parse_args())
-    if args['action'] == 'track':
+    
+    args = parser.parse_args()
+    if  args.action == Action.track:
         
-        database.add_record_to_database(args['project'], args['min'])
-    elif args['action'] == 'stat':
-        
-        statistic_data = database.get_statistic_data_from_database(args['project'], args['days'])
+        database.add_record_to_database(args.project, args.min)
+    else:        
+        statistic_data = database.get_statistic_data_from_database(args.project, args.days)
         show_statistic_data(statistic_data)
-    else:
-        print('Не верное значение аргумента action')
-        parser.print_help()
